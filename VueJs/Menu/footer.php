26460fce1d79f07f2551aec2dcb4051937a792bb
Marco Fernandes Ferreira <marco.nine@hotmail.com>
2018-02-21 11:29:47 +0100

    GES-93 Done + quelques modifications de responsivite

--- VueJs/Menu/footer.php
+++ VueJs/Menu/footer.php
@@ -3,19 +3,18 @@
     <link rel="stylesheet" href="MyGe-Soifcss.css">
 </head>
 <footer>
-    
 
     <p class="text-center">© Copyright, Ge-Soif.ch</p>
     
 
  
- <div class="text-center">
+ <div class=" text-center col-sm-offset-3 col-lg-offset-4 col-md-offset-3 col-xs-12 col-sm-9 col-lg-9 col-md-9">
      <div class="visible-lg visible-md hidden-xs hidden-sm">
-         <iframe id="IcooneFb" src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fgenevasoif%2F&width=450&layout=standard&action=like&size=small&show_faces=true&share=true&height=80&appId" width="650" height="40" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>     
+         <iframe id="IcooneFb" src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fgenevasoif%2F&width=450&layout=standard&action=like&size=small&show_faces=true&share=true&height=80&appId" width="100%" height="40" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>     
      </div>
      
      <div class="visible-xs visible-sm hidden-lg hidden-md">
-         <iframe id="IcooneFb" src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fgenevasoif%2F&width=450&layout=standard&action=like&size=small&show_faces=true&share=true&height=80&appId" width="650" height="40" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>     
+         <iframe id="IcooneFb" src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fgenevasoif%2F&width=450&layout=standard&action=like&size=small&show_faces=true&share=true&height=80&appId" width="100%" height="40" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>     
         </div>
    
  </div>
