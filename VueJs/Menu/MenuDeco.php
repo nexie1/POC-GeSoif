8d98354e62e98853cf8576a527109b058ea32b4d
DanySer <31500313+DanySer@users.noreply.github.com>
2018-03-07 14:58:16 +0100

    GES-95,96: En test

--- VueJs/Menu/MenuDeco.php
+++ VueJs/Menu/MenuDeco.php
@@ -7,16 +7,18 @@
 
             <a  v-bind:href="link"> <img class="littleImg" id="iconNavbar" src="./img/faviconGlyphicon.png" alt="logo" /></a>
             <a class="navbar-brand"  v-bind:href="link" style="color : white;">{{message}}</a>
-        
+
         </div>
         <div id="navInfo" class="navbar-collapse collapse" style="float: right;">
             <ul>
-                <li class="nav navbar-form navbar-left" aria-label="Left Align" ><a href="./about.php" style="color : white"><span class="glyphicon glyphicon-info-sign" aria-hidden="true" ></span> A propos</a></li>
-                <a href="https:\\www.facebook.com/genevasoif/"><img class="Imgsz" id="FbIcoone" alt="image" src="./img/facebook.png"/></a>
-                <a href="https://www.instagram.com/gesoif_officiel/"><img class="Imgsz" id="InstIcoone" alt="image" src="./img/instagram.png"/></a>
-                <a href="https://youtu.be/K3JngtvrcdI"><img class="Imgsz" id="YtbIcoone" alt="image" src="./img/youtube.png"/></a>
+              <li class="nav navbar-form navbar-left" aria-label="Left Align" ><a href="./about.php" style="color : white"><span class="glyphicon glyphicon-info-sign"  aria-hidden="true"></span> A propos</a></li>
             </ul>
-        </div>  
+        </div>
+        <div id="reseauxSociaux">
+          <a href="https:\\www.facebook.com/genevasoif/"><img class="Imgsz" id="FbIcoone" alt="image" src="./img/facebook.png"/></a>
+          <a href="https://www.instagram.com/gesoif_officiel/"><img class="Imgsz" id="InstIcoone" alt="image" src="./img/instagram.png"/></a>
+          <a href="https://youtu.be/K3JngtvrcdI"><img class="Imgsz" id="YtbIcoone" alt="image" src="./img/youtube.png"/></a>
+        </div>
     </div>
 
 </nav>
